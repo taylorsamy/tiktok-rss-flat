a886f8b437989f680af60314d47b2c04c02b9cb3
Taylor Amy <taylorsamy@users.noreply.github.com>
2022-03-16 12:25:36 -0600

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -7,7 +7,7 @@ from datetime import datetime, timezone
 # ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
 
 # Custom Domain
-ghPagesURL = "https://tiktokrss.conoroneill.com/"
+ghPagesURL = "https://taylorsamy.github.io/tiktok-rss-flat/"
 
 api = TikTokApi()
 
